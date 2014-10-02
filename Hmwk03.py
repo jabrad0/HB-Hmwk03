81f2b66b8e461b09b108796e8b3e610824132842
JABbook <puff@Streamborns-MacBook-Pro.local>
2014-10-01 22:20:53 -0700

    Hmwk03 with functions

--- Hmwk03.py
+++ Hmwk03.py
@@ -1,7 +1,5 @@
-def main():
-    print "******************************************"
-    f = open("orders_by_type.csv")
-    melon_tallies = {"Musk": 0, "Hybrid": 0, "Watermelon": 0, "Winter": 0}
+def print_melon_tallies(filename, melon_tallies):
+    f = open(filename)
     for line in f:
         data = line.split(",")
         melon_type = data[1]
@@ -14,17 +12,21 @@ def main():
         price = melon_prices[melon_type]
         revenue = price * melon_tallies[melon_type]
         total_revenue += revenue
+        
         print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
-    print "******************************************"
-    f = open("orders_with_sales.csv")
+    
+def print_sales(filename):
+    f = open(filename)
     sales = [0, 0]
     for line in f:
         data = line.split(",")
-        #Changed line below to this from Data[1] == 0 
+        #Changed line below from 'if data[1] == 0' to: 
         if data[2] == "Online":
             sales[0] += float(data[3])
         else:
             sales[1] += float(data[3])
+
+    print "******************************************"
     print "Salespeople generated %0.2f in revenue." % sales[1]
     print "Internet sales generated %0.2f in revenue." % sales[0]
     if sales[1] > sales[0]:
@@ -32,7 +34,14 @@ def main():
     else:
         print "Time to fire the sales team! Online sales rule all!"
     print "******************************************"
-    
 
+
+def main():
+    print "******************************************"
+    melon_tallies = {"Musk": 0, "Hybrid": 0, "Watermelon": 0, "Winter": 0}
+    print_melon_tallies("orders_by_type.csv", melon_tallies)
+
+    print_sales("orders_with_sales.csv")
+    
 if __name__ == "__main__":
     main()
